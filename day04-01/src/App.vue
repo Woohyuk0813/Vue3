006ef0df27498f763872436acdf98e92432d04f0
Woohyuk0813 <wh081312@gmail.com>
2025-11-20 11:16:29 +0900

    1232

--- day04-01/src/App.vue
+++ day04-01/src/App.vue
@@ -6,6 +6,7 @@
 // import WatchEffectEx1 from './components/WatchEffectEx1.vue';
 // import WatchPostEffect from './components/WatchPostEffectEx1.vue';
 import { reactive, ref } from 'vue';
+import DefineProps from './components/DefineProps.vue';
 const count = ref(0);
 const state = reactive({
   count: 0,
@@ -21,3 +22,5 @@ const state = reactive({
   <WatchPostEffect /> -->
   <DefineProps :count="count" :state="state" />
 </template>
+
+<!-- DefineProps 컴포넌트에서는 부모(App.vue)에서 전달한 반응형 데이터를 defineProps() 함수를 사용하여 받아서 출력할 수 있다 -->
